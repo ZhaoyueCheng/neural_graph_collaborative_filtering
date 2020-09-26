aa7b351a89c661610fb0020940ed4568c04924cd
Joey <joey@layer6.ai>
2020-09-26 16:55:37 -0400

    added electronics dataset, update config

--- NGCF/utility/parser.py
+++ NGCF/utility/parser.py
@@ -16,7 +16,7 @@ def parse_args():
     parser.add_argument('--proj_path', nargs='?', default='',
                         help='Project path.')
 
-    parser.add_argument('--dataset', nargs='?', default='Amazon-Book',
+    parser.add_argument('--dataset', nargs='?', default='yelp',
                         help='Choose a dataset from {gowalla, yelp2018, amazon-book}')
     parser.add_argument('--pretrain', type=int, default=0,
                         help='0: No pretrain, -1: Pretrain with the learned embeddings, 1:Pretrain with stored models.')
@@ -27,14 +27,14 @@ def parse_args():
 
     parser.add_argument('--embed_size', type=int, default=50,
                         help='Embedding size.')
-    parser.add_argument('--layer_size', nargs='?', default='[64]',
+    parser.add_argument('--layer_size', nargs='?', default='[64, 64, 64]',
                         help='Output sizes of every layer')
     parser.add_argument('--batch_size', type=int, default=1024,
                         help='Batch size.')
 
-    parser.add_argument('--regs', nargs='?', default='[1e-5,1e-5,1e-2]',
+    parser.add_argument('--regs', nargs='?', default='[1e-5]',
                         help='Regularizations.')
-    parser.add_argument('--lr', type=float, default=0.01,
+    parser.add_argument('--lr', type=float, default=0.0001,
                         help='Learning rate.')
 
     parser.add_argument('--model_type', nargs='?', default='ngcf',
@@ -51,7 +51,7 @@ def parse_args():
                         help='0: Disable node dropout, 1: Activate node dropout')
     parser.add_argument('--node_dropout', nargs='?', default='[0.1]',
                         help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
-    parser.add_argument('--mess_dropout', nargs='?', default='[0.1]',
+    parser.add_argument('--mess_dropout', nargs='?', default='[0.1, 0.1, 0.1]',
                         help='Keep probability w.r.t. message dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
 
     parser.add_argument('--Ks', nargs='?', default='[10, 20]',
